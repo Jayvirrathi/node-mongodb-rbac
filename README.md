44b3ad8907712c8a2fb3cdfdc7bcf1626d5c1986
Jayvir Rathi <jayvirrathi@gmail.com>
2021-03-13 18:27:51 +0530

    first commit 🎉

---
+++ README.md
@@ -0,0 +1,19 @@
+# RBAC REST API with Node.js, MongoDB , Passport & Session
+
+> Role Based Access Control project
+
+## Build Setup
+
+```bash
+# install dependencies
+npm install
+
+# serve at http://localhost:4000/
+
+npm run dev
+```
+
+## Prerequisites
+
+- Nodejs
+- MongoDB
---
+++ src/index.js
@@ -0,0 +1,131 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+dotenv.config();
+const createHttpError = require('http-errors');
+const session = require('express-session');
+const connectFlash = require('connect-flash');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
+const { ensureLoggedIn } = require('connect-ensure-login');
+const { roles } = require('./utils/constants');
+
+// Initialization
+const app = express();
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 4000;
+
+// Making a connection to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    bufferCommands: false,
+    bufferMaxEntries: 0,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log('💾 connected...');
+    // Listening for connections on the defined PORT
+    app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+  })
+  .catch((err) => console.log(err.message));
+
+// Init Session
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      // secure: true,
+      httpOnly: true
+    },
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
+  })
+);
+
+// For Passport JS Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+require('./utils/passport.auth');
+
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+// Connect Flash
+app.use(connectFlash());
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.route'));
+app.use('/auth', require('./routes/auth.route'));
+app.use(
+  '/user',
+  ensureLoggedIn({ redirectTo: '/auth/login' }),
+  require('./routes/user.route')
+);
+app.use(
+  '/admin',
+  ensureLoggedIn({ redirectTo: '/auth/login' }),
+  ensureAdmin,
+  require('./routes/admin.route')
+);
+
+// 404 Handler
+app.use((req, res, next) => {
+  next(createHttpError.NotFound());
+});
+
+// Error Handler
+app.use((error, req, res, next) => {
+  error.status = error.status || 500;
+  res.status(error.status);
+  res.render('error_40x', { error });
+});
+
+// Setting the PORT
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     next();
+//   } else {
+//     res.redirect('/auth/login');
+//   }
+// }
+
+function ensureAdmin(req, res, next) {
+  if (req.user.role === roles.admin) {
+    next();
+  } else {
+    req.flash('warning', 'you are not Authorized to see this route');
+    res.redirect('/');
+  }
+}
+
+function ensureModerator(req, res, next) {
+  if (req.user.role === roles.moderator) {
+    next();
+  } else {
+    req.flash('warning', 'you are not Authorized to see this route');
+    res.redirect('/');
+  }
+}
